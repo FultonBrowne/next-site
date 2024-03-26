ac5d6d1e76341a12756c6138feff19c3563108e5
Fulton Browne <fultonbrowne@pm.me>
2024-03-25 20:29:54 -0500

    new site

---
+++ Dockerfile
@@ -0,0 +1,44 @@
+# Step 1: Build your Next.js application
+# Use the official lightweight Node.js 16 image.
+# https://hub.docker.com/_/node
+FROM node:16-alpine as builder
+
+# Set the working directory
+WORKDIR /app
+
+# Copy package.json and package-lock.json (or yarn.lock if you use yarn)
+COPY package*.json ./
+# If you're using yarn with a `yarn.lock` file, uncomment the next line
+# COPY yarn.lock ./
+
+# Install dependencies
+RUN npm install
+# If you're using yarn, run the following command instead
+# RUN yarn install
+
+# Copy the rest of your app's source code from your host to your image filesystem.
+COPY . .
+
+# Build your Next.js application for production.
+RUN npm run build
+
+# Step 2: Serve the Next.js application using a Node.js server
+# Use the official lightweight Node.js 16 image.
+FROM node:16-alpine
+
+# Set the working directory
+WORKDIR /app
+
+# Copy the built Next.js application from the previous stage
+COPY --from=builder /app/.next ./.next
+COPY --from=builder /app/public ./public
+COPY --from=builder /app/package.json ./package.json
+
+# Install production dependencies only
+RUN npm install next
+
+# Expose the port the app runs on
+EXPOSE 3000
+
+# Run your Next.js app
+CMD ["npm", "start"]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,84 +14,77 @@ const kantit = Kanit({
 });
 
 export default function Home() {
-  const nextPaneRef = useRef(null); // Create a ref for the next pane
 
   const scrollToNextPane = () => {
-    // Scroll to the next pane using the ref
-    window.scrollTo({
-      top: nextPaneRef.current.offsetTop,
-      behavior: "smooth",
-    });
-  };
 
-  return (
-    <main className={`${kantit.className}`}>
-      <div
-        className={`flex min-h-screen items-center p-4 md:justify-start md:p-24`}
-      >
-        <div className="w-full md:w-1/3">
-          <div className="text-7xl md:text-9xl">
-            Hello <br /> there,
+
+    return (
+      <main className={`${kantit.className}`}>
+        <div
+          className={`flex min-h-screen items-center p-4 md:justify-start md:p-24`}
+        >
+          <div className="w-full md:w-1/3">
+            <div className="text-7xl md:text-9xl">
+              Hello <br /> there,
+            </div>
+            <div className="mt-4 text-xl">I{"'"}m Fulton Browne</div>
+            <div className="mt-4 flex gap-2 text-xl">
+              <FaGithubAlt
+                onClick={() => window.open("https://github.com/FultonBrowne")}
+              />
+              <FaTwitter
+                onClick={() => window.open("https://twitter.com/BrowneFulton")}
+              />
+              <FaLinkedin
+                onClick={() =>
+                  window.open(
+                    "https://www.linkedin.com/in/fulton-browne-925a20245/",
+                  )
+                }
+              />
+            </div>
           </div>
-          <div className="mt-4 text-xl">I{"'"}m Fulton Browne</div>
-          <div className="mt-4 flex gap-2 text-xl">
-            <FaGithubAlt
-              onClick={() => window.open("https://github.com/FultonBrowne")}
-            />
-            <FaTwitter
-              onClick={() => window.open("https://twitter.com/BrowneFulton")}
-            />
-            <FaLinkedin
-              onClick={() =>
-                window.open(
-                  "https://www.linkedin.com/in/fulton-browne-925a20245/",
-                )
-              }
+          <div className="absolute bottom-16 left-1/2 -translate-x-1/2 transform">
+            <FaArrowDown
+              className="animate-bounce text-3xl"
+              onClick={scrollToNextPane}
             />
           </div>
         </div>
-        <div className="absolute bottom-16 left-1/2 -translate-x-1/2 transform">
-          <FaArrowDown
-            className="animate-bounce text-3xl"
-            onClick={scrollToNextPane}
-          />
-        </div>
-      </div>
-      <div
-        ref={nextPaneRef}
-        className={`flex min-h-screen w-full content-center justify-center p-4 md:p-24`}
-      >
-        <div className="flex flex-col content-center justify-center gap-4 md:gap-16">
-          <div className="w-full justify-center text-5xl md:text-7xl">
-            I{"'"}m passionate <br />
-            about...{" "}
-          </div>
-          <div className="text-lg md:text-lg">
-            All things tech - from software development to hardware hacking.{" "}
-            <br />
-            Particularly I work enjoy systems programming and low-level <br />
-            development, AI development, and web development. I{"'"}ve worked on{" "}
-            <br />
-            several open source projects in those spaces and between that and my
-            <br />
-            professional work I{"'"}ve gained a lot of experience in variety of
-            <br />
-            areas and technolgies. Specifically:
-            <ul className="list-inside list-disc pt-8">
-              <li>Systems Programming: with C, Rust, Go, and Ocaml</li>
-              <li>AI: with Python, pytorch, LlamaCPP and the HF framework</li>
-              <li>
-                Web Dev: in countless frameworks including Django, <br />
-                Axum, Spring, and flask
-              </li>
-              <li>A bit of frontend: with NextJS, remix and react</li>
-            </ul>
+        <div
+          className={`flex min-h-screen w-full content-center justify-center p-4 md:p-24`}
+        >
+          <div className="flex flex-col content-center justify-center gap-4 md:gap-16">
+            <div className="w-full justify-center text-5xl md:text-7xl">
+              I{"'"}m passionate <br />
+              about...{" "}
+            </div>
+            <div className="text-lg md:text-lg">
+              All things tech - from software development to hardware hacking.{" "}
+              <br />
+              Particularly I work enjoy systems programming and low-level <br />
+              development, AI development, and web development. I{"'"}ve worked on{" "}
+              <br />
+              several open source projects in those spaces and between that and my
+              <br />
+              professional work I{"'"}ve gained a lot of experience in variety of
+              <br />
+              areas and technolgies. Specifically:
+              <ul className="list-inside list-disc pt-8">
+                <li>Systems Programming: with C, Rust, Go, and Ocaml</li>
+                <li>AI: with Python, pytorch, LlamaCPP and the HF framework</li>
+                <li>
+                  Web Dev: in countless frameworks including Django, <br />
+                  Axum, Spring, and flask
+                </li>
+                <li>A bit of frontend: with NextJS, remix and react</li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="bg-white font-sans text-black">
-        not much of a designer tho
-      </div>
-    </main>
-  );
-}
+        <div className="bg-white font-sans text-black">
+          not much of a designer tho
+        </div>
+      </main>
+    );
+  }
