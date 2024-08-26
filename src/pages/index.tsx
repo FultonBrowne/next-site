21d36ff250df68c3ef6b308417e4e24920e48153
Fulton Browne <fultonbrowne@pm.me>
2024-08-26 09:28:21 -0500

    minior changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,79 +4,59 @@ import {
   FaArrowDown,
   FaGithubAlt,
   FaLinkedin,
+  FaRss,
   FaTwitter,
 } from "react-icons/fa";
-import { Kanit } from "next/font/google";
+import { Kanit, Open_Sans } from "next/font/google";
+import { FaBlog } from "react-icons/fa6";
 
 const kantit = Kanit({
   subsets: ["latin"],
   weight: "700",
 });
-
+const openSans = Open_Sans({
+  subsets: ["latin"],
+});
 export default function Home() {
   return (
-    <main className={`${kantit.className}`}>
+    <main className={`${openSans.className}`}>
       <div
-        className={`flex min-h-screen items-center p-4 md:justify-start md:p-24`}
+        className={`flex min-h-screen items-center p-4 justify-center md:justify-start md:p-24`}
       >
-        <div className="w-full md:w-1/3">
-          <div className="text-7xl md:text-9xl">
-            Hello <br /> there,
+        <div className="w-full text-center lg:text-start lg:w-1/3">
+          <div className={`text-7xl md:text-9xl ${kantit.className}`}>
+            Hello <br className="hidden lg:block" /> there,
           </div>
-          <div className="mt-4 text-xl">I{"'"}m Fulton Browne</div>
-          <div className="mt-4 flex gap-2 text-xl">
+          <div className="mt-4 text-xs md:text-xl">
+            I{"'"}m <span className="title italic">Fulton Browne,</span>
+            <br /> A student and software engineer looking to make the world a
+            better place
+          </div>
+          <div className="mt-4 flex gap-2  text-xl">
             <FaGithubAlt
+              className="cursor-pointer"
               onClick={() => window.open("https://github.com/FultonBrowne")}
             />
             <FaTwitter
+              className="cursor-pointer"
               onClick={() => window.open("https://twitter.com/BrowneFulton")}
             />
             <FaLinkedin
+              className="cursor-pointer"
               onClick={() =>
                 window.open(
                   "https://www.linkedin.com/in/fulton-browne-925a20245/",
                 )
               }
             />
+            <FaRss
+              className="cursor-pointer"
+              onClick={() =>
+                window.open("https://fultonsramblings.substack.com/")
+              }
+            />
           </div>
         </div>
-        <div className="absolute bottom-16 left-1/2 -translate-x-1/2 transform">
-          <FaArrowDown className="animate-bounce text-3xl" />
-        </div>
-      </div>
-      <div
-        className={`flex min-h-screen w-full content-center justify-center p-4 md:p-24`}
-      >
-        <div className="flex flex-col content-center justify-center gap-4 md:gap-16">
-          <div className="w-full justify-center text-5xl md:text-7xl">
-            I{"'"}m passionate <br />
-            about...{" "}
-          </div>
-          <div className="text-lg md:text-lg">
-            All things tech - from software development to hardware hacking.{" "}
-            <br />
-            Particularly I work enjoy systems programming and low-level <br />
-            development, AI development, and web development. I{"'"}ve worked on{" "}
-            <br />
-            several open source projects in those spaces and between that and my
-            <br />
-            professional work I{"'"}ve gained a lot of experience in variety of
-            <br />
-            areas and technolgies. Specifically:
-            <ul className="list-inside list-disc pt-8">
-              <li>Systems Programming: with C, Rust, Go, and Ocaml</li>
-              <li>AI: with Python, pytorch, LlamaCPP and the HF framework</li>
-              <li>
-                Web Dev: in countless frameworks including Django, <br />
-                Axum, Spring, and flask
-              </li>
-              <li>A bit of frontend: with NextJS, remix and react</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className="bg-white font-sans text-black">
-        not much of a designer tho
       </div>
     </main>
   );
